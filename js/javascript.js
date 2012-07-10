3d43878756a3b8514f9fc8f8d3671761341f1287
Chris P. Ambrose <chrispambrose@gmail.com>
2012-07-10 14:12:14 -0400

    Moved stooges array out to global space

--- js/javascript.js
+++ js/javascript.js
@@ -3,9 +3,9 @@ $(document).ready(function() {
   console.log(groups);
 });
 
+var stooges = [{name : 'moe', age : 40}, {name : 'larry', age : 50}, {name : 'curly', age : 55}];
 
 function get_groups() {
-  var stooges = [{name : 'moe', age : 40}, {name : 'larry', age : 50}, {name : 'curly', age : 55}];
   var groups = _.groupBy(stooges, function(item) {
     if(item.age < 50) {
       return '40s';
