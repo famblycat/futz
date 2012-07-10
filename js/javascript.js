86fd3d781a4c5e5d5ea30d96655f5ba5da2fb8cf
Chris P. Ambrose <chrispambrose@gmail.com>
2012-07-10 14:08:05 -0400

    Just cleaned up some stuff

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
+  var groups = get_groups();
+  console.log(groups);
+});
 
+
+function get_groups() {
+  var stooges = [{name : 'moe', age : 40}, {name : 'larry', age : 50}, {name : 'curly', age : 55}];
   var groups = _.groupBy(stooges, function(item) {
     if(item.age < 50) {
       return '40s';
@@ -9,9 +15,5 @@ $(document).ready(function() {
       return '60s';
     }
   });
-
-  console.log(groups);
-});
-
-var stooges = [{name : 'moe', age : 40}, {name : 'larry', age : 50}, {name : 'curly', age : 55}];
+}
 
